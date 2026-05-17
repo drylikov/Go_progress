0eacd8c7fe7e7938763fdef648df3ebd5dc2ddc9
Denis Rylikov <63297132+drylikov@users.noreply.github.com>
2026-05-17 23:35:41 +0200

    Go_progress
    Another Go progress bar.

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/drylikov/go_progress"
+	"github.com/drylikov/go/term"
+
+	color "github.com/aybabtme/rgbterm"
+)
+
+// gray string.
+func gray(s string) string {
+	return color.FgString(s, 150, 150, 150)
+}
+
+// purple string.
+func purple(s string) string {
+	return color.FgString(s, 96, 97, 190)
+}
+
+func main() {
+	term.HideCursor()
+	defer term.ShowCursor()
+
+	b := progress.NewInt(10)
+	b.Width = 40
+	b.StartDelimiter = gray("|")
+	b.EndDelimiter = gray("|")
+	b.Filled = purple("█")
+	b.Empty = gray("░")
+
+	render := term.Renderer()
+
+	for i := 0; i <= 10; i++ {
+		b.ValueInt(i)
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		render(term.CenterLine(b.String()))
+	}
+
+	term.ClearAll()
+}
